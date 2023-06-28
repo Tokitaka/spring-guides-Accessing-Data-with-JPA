412b5d7f1f398fed329c649c50b28c4ac935dd1c
tokitaka <artigen3@gmail.com>
2023-06-29 02:24:55 +0900

    Customer java entity

--- src/main/java/com/example/acessingdatajpa/Customer.java
+++ src/main/java/com/example/acessingdatajpa/Customer.java
@@ -1,5 +1,37 @@
 package com.example.acessingdatajpa;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Customer {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    protected Customer(){}
+    // id는 db가 생성 
+    public Customer(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    //Customer 객체 정보를 string으로 확인 가능 
+    @Override
+    public String toString(){
+        return String.format("Customer[id='%d', firstName='%s', lastName='%s']", id, firstName, lastName);
+    }
+    public int getId() {
+        return id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
 }
