5ac4104cd6b8247f08b0040d10edd7253c615eeb
tokitaka <artigen3@gmail.com>
2023-06-29 18:07:37 +0900

    done

--- src/main/java/com/example/acessingdatajpa/AcessingDataJpaApplication.java
+++ src/main/java/com/example/acessingdatajpa/AcessingDataJpaApplication.java
@@ -1,13 +1,36 @@
 package com.example.acessingdatajpa;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AcessingDataJpaApplication {
+	private static final Logger log = LoggerFactory.getLogger(AcessingDataJpaApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AcessingDataJpaApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+
+
+			log.info("Customers found with findAll()");
+			log.info("-------------------------------");
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+		};
 	}
 
 }
